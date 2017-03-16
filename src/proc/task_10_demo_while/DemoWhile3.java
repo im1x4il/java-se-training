6255db8121370b8ec433df4531bf8fb29c650388
Михаил <>
2017-03-16 18:51:11 +0200

    робоча вепсия

---
+++ src/proc/task_10_demo_while/DemoWhile3.java
@@ -0,0 +1,36 @@
+package proc.task_10_demo_while;
+
+import java.util.Scanner;
+
+/**
+ * Created by Михаил on 15.03.2017.
+ */
+public class DemoWhile3 {
+    public static void main(String[] args) {
+        Scanner s;
+        s = new Scanner(System.in);
+        int i = s.nextInt();
+        int g = s.nextInt();
+        int index=0;
+        int array [] = new int[1000];
+        if (i < g) {
+            while (i != g) {
+                index++;
+                if (index %2==0) {
+                    g--;
+                    array[index]=g;
+                }
+                System.out.println(array[index]);
+            }
+        } else {
+            while (i != g) {
+                index++;
+                if (index%2==0){
+                    g++;
+                    array[index]=g;
+                }
+                System.out.println(array[index]);
+            }
+        }
+    }
+}
