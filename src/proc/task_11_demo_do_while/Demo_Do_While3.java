d35f493c5556b2687d8ed1480011685e9b765f76
Михаил <>
2017-03-17 12:00:46 +0200

    робоча вепсия

--- src/proc/task_11_demo_do_while/Demo_Do_While3.java
+++ src/proc/task_11_demo_do_while/Demo_Do_While3.java
@@ -1,36 +1,36 @@
-package proc.task_10_demo_while;
+package proc.task_11_demo_do_while;
 
 import java.util.Scanner;
 
 /**
- * Created by Михаил on 15.03.2017.
+ * Created by Михаил on 17.03.2017.
  */
-public class DemoWhile3 {
+public class Demo_Do_While3 {
     public static void main(String[] args) {
         Scanner s;
         s = new Scanner(System.in);
         int i = s.nextInt();
         int g = s.nextInt();
-        int index=0;
-        int array [] = new int[1000];
+        int index = 0;
+        int array[] = new int[1000];
         if (i < g) {
-            while (i-1 != g) {
+            do {
                 index++;
-                if (index %2==0) {
-                    array[index]=g;
+                if (index % 2 == 0) {
+                    array[index] = g;
                     g--;
                 }
                 System.out.println(array[index]);
-            }
+            } while (i != g);
         } else {
-            while (i != g) {
+            do {
                 index++;
-                if (index%2==0){
-                    array[index]=g;
+                if (index % 2 == 0) {
+                    array[index] = g;
                     g++;
                 }
                 System.out.println(array[index]);
-            }
+            } while (i - 1 != g);
         }
     }
 }
